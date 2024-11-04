78d04a166e7b827fd997b1f73f660e5976938245
mhelala <mariam.helala@enovea.net>
2024-11-04 14:42:31 +0100

    Before rebase

--- src/main/kotlin/net/enovea/domain/driver/DriverMapper.kt
+++ src/main/kotlin/net/enovea/domain/driver/DriverMapper.kt
@@ -1,16 +1,29 @@
 package net.enovea.domain.driver
 
+import net.enovea.domain.team.TeamSummaryMapper
+import net.enovea.domain.vehicle.VehicleTeamEntity
 import net.enovea.dto.DriverDTO
+import net.enovea.dto.TeamDTOsummary
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import org.mapstruct.factory.Mappers
 
 @Mapper
 interface DriverMapper {
     // Map from DriverEntity to DriverDTO
+    @Mapping(source ="driverTeams",target = "team")
     fun toDto(driver: DriverEntity): DriverDTO
 
     // Map from DriverDTO to DriverEntity
     fun toEntity(driverDTO: DriverDTO): DriverEntity
+
+    //Map the most recent team to TeamDTO
+    fun mapMostRecentTeam(driverTeams: List<DriverTeamEntity>): TeamDTOsummary? {
+        return driverTeams
+            .filter { it.endDate == null }
+            .maxByOrNull { it.id.startDate }
+            ?.let { TeamSummaryMapper.INSTANCE.toDto(it.team!!) }
+    }
 
     companion object {
         val INSTANCE: DriverMapper = Mappers.getMapper(DriverMapper::class.java)
--- src/main/kotlin/net/enovea/dto/DriverDTO.kt
+++ src/main/kotlin/net/enovea/dto/DriverDTO.kt
@@ -1,13 +1,10 @@
 package net.enovea.dto
 
-import net.enovea.domain.vehicle.TimestampRange
-
-
 data class DriverDTO (
     val id : Int,
     val firstName: String,
     val lastName: String,
     val phoneNumber: String?,
     val team: TeamDTOsummary,
-    val teams: Map<TimestampRange, TeamDTO>? = null
+    //val driverTeams: Map<TimestampRange, TeamDTO>? = null
 )

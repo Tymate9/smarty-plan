6dc255a6e6dd571cbf4624ab072314b86a7deee4
iesmaili <ilyes.esmaili@enovea.net>
2024-12-04 13:49:40 +0100

    Add Logs formatting

--- src/main/kotlin/net/enovea/conf/SmsServiceConfiguration.kt
+++ src/main/kotlin/net/enovea/conf/SmsServiceConfiguration.kt
@@ -1,10 +1,12 @@
 package net.enovea.conf
 
+import jakarta.enterprise.context.Dependent
 import net.enovea.api.sms.SmsService
 import jakarta.enterprise.inject.Produces
 import jakarta.inject.Named
 import org.eclipse.microprofile.config.inject.ConfigProperty
 
+@Dependent
 class SmsServiceConfiguration {
     @Produces
     @Named("smsService")

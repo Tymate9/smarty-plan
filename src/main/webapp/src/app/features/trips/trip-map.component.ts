05f4368cbb8fe0b988cf8dada15aa5f11b0978b0
BERNARD Yoann <ybernard@enovea.net>
2025-04-07 13:30:21 +0200

    Ceil to min 1 & round

--- src/main/webapp/src/app/features/trips/trip-map.component.ts
+++ src/main/webapp/src/app/features/trips/trip-map.component.ts
@@ -40,7 +40,7 @@ import TripEventType = dto.TripEventType;
               <p>{{ tripsService.formatDuration(tripData!.idleDuration) }}</p>
             </p-card>
             <p-card header="Distance totale">
-              <p>{{ Math.ceil(tripData!.drivingDistance) }} Km</p>
+              <p>{{ Math.max(1, tripData!.drivingDistance).toFixed(0) }} Km</p>
             </p-card>
             <p-card header="Nb de POI visités">
               <p>{{ tripData!.poiAmount }}</p>
@@ -137,7 +137,7 @@ import TripEventType = dto.TripEventType;
 
                     <div class="distance-rectangle small-right" style="position: relative">
                       <p
-                        style="position: absolute; top:-0.6rem; left:0.2rem;">{{ Math.ceil(event.originalEvent.distance) }}
+                        style="position: absolute; top:-0.6rem; left:0.2rem;">{{ Math.max(1, event.originalEvent.distance).toFixed(0) }}
                         Km</p>
                       <i *ngIf="event.originalEvent.sourceIndexes?.length > 0" class="pi pi-star-fill"
                          style="bottom:0.4rem; right:0rem; position: absolute; color: darkred;"></i>
@@ -278,7 +278,7 @@ import TripEventType = dto.TripEventType;
                     </span>
 
                     <div class="distance-rectangle small-right">
-                      {{ Math.ceil(event.originalEvent.distance) }} Km
+                      {{ Math.max(1, event.originalEvent.distance).toFixed(0) }} Km
                     </div>
                   </div>
                   <div
